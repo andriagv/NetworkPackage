7d02e6884f1a24e250389e75328655ea00a784fc
andria.gv <gvaramiaandria1@gmail.com>
2024-12-04 22:32:59 +0400

    add postData function

--- Sources/NetworkPackage/NetworkPackage.swift
+++ Sources/NetworkPackage/NetworkPackage.swift
@@ -8,13 +8,13 @@ public enum NetworkError: Error {
     case statusCodeError(statusCode: Int)
     case noData
     case decodeError(error: Error)
+    case encodingError
+    case unknownError
 }
 
-
 public final class NetworkService: NetworkServiceProtocol {
-    
-    public init() { }
-    
+    public init() {}
+
     @available(iOS 15, macOS 12.0, *)
     public func fetchData<T: Codable>(urlString: String, headers: [String: String]? = nil) async throws -> T {
         guard let url = URL(string: urlString) else {
@@ -22,6 +22,7 @@ public final class NetworkService: NetworkServiceProtocol {
         }
         
         var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
         
         if let headers = headers {
             for (key, value) in headers {
@@ -42,9 +43,50 @@ public final class NetworkService: NetworkServiceProtocol {
         do {
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-            
-            let fetchedData = try decoder.decode(T.self, from: data)
-            return fetchedData
+            let decodedData = try decoder.decode(T.self, from: data)
+            return decodedData
+        } catch {
+            throw NetworkError.decodeError(error: error)
+        }
+    }
+
+    @available(iOS 15, macOS 12.0, *)
+    public func postData<T: Codable>(urlString: String, body: [String: Any], headers: [String: String]? = nil) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let headers = headers {
+            for (key, value) in headers {
+                urlRequest.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        do {
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+        } catch {
+            throw NetworkError.encodingError
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.httpResponseError
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.statusCodeError(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(T.self, from: data)
+            return decodedData
         } catch {
             throw NetworkError.decodeError(error: error)
         }
--- Sources/NetworkPackage/NetworkServiceProtocol.swift
+++ Sources/NetworkPackage/NetworkServiceProtocol.swift
@@ -6,7 +6,14 @@
 //
 
 
-@available(iOS 15, macOS 12.0, *)
+import Foundation
+
 public protocol NetworkServiceProtocol {
+    @available(iOS 15, macOS 12.0, *)
     func fetchData<T: Codable>(urlString: String, headers: [String: String]?) async throws -> T
+
+    @available(iOS 15, macOS 12.0, *)
+    func postData<T: Codable>(urlString: String, body: [String: Any], headers: [String: String]?) async throws -> T
 }
+
+
