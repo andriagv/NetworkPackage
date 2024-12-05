900af358afa9de9ebed0bf9f1c4cc54918957ea0
andria.gv <gvaramiaandria1@gmail.com>
2024-12-05 19:47:39 +0400

    add Raw JSON Response

--- Sources/NetworkPackage/NetworkPackage.swift
+++ Sources/NetworkPackage/NetworkPackage.swift
@@ -51,6 +51,10 @@ public final class NetworkService: NetworkServiceProtocol {
         
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
+        if let rawJSON = String(data: data, encoding: .utf8) {
+            print("Raw JSON Response: \(rawJSON)")
+        }
+        
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.httpResponseError
         }
